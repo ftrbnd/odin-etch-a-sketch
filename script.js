42bfe4d9ffbdd4e8db22f0957feb60db77ed46e3
ftrbnd <giosalas25@gmail.com>
2023-01-27 12:28:21 -0800

    Add hover effect on cells

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const containerDiv = document.querySelector('#container');
+const containerDiv = document.querySelector('#cells-container');
+
 for (let i = 0; i < 16; i++) {
     const rowDiv = document.createElement('div');
     rowDiv.setAttribute('class', 'row');
+    rowDiv.style.backgroundColor = 'darkgray';
+
     for (let j = 0; j < 16; j++) {
         const cellDiv = document.createElement('div');
         cellDiv.setAttribute('class', 'cell');
         cellDiv.style.height = '40px';
         cellDiv.style.width = '40px';
-        cellDiv.style.margin = '1px';
-        cellDiv.style.border = '1px dotted black';
+        cellDiv.style.border = '1px dotted gray';
+
         rowDiv.appendChild(cellDiv);
     }
     containerDiv.appendChild(rowDiv);
+}
+
+const cells = document.querySelectorAll('div.cell');
+for (const cell of cells) {
+    cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = '#2E2E2E'; // dark gray
+    });
 }
