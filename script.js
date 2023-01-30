5005b4f9584eca38543a446dd9d43f637305ac70
ftrbnd <giosalas25@gmail.com>
2023-01-30 13:20:29 -0800

    Fix resizing

--- script.js
+++ script.js
@@ -1,57 +1,43 @@
+const container = document.querySelector('.container');
+const customizeButton = document.querySelector('button#customize');
+
 createPage();
 
 function createPage() {
     loadCells();
 
-    const customizeButton = document.querySelector('button#customize');
     attachButtonListeners(customizeButton);
-    
 }
 
-function loadCells(sideLength = 16, userSelected = false) {
-    const containerDiv = document.querySelector('#cells-container');
-
-    if (userSelected) {
-        const cells = document.querySelectorAll('div.cell');
-        for (const cell of cells) {
-            cell.remove();
-        }
+function loadCells(sideLength = 16, userSelected = true) {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
     }
 
     for (let i = 0; i < sideLength; i++) {
-        const rowDiv = document.createElement('div');
-        rowDiv.setAttribute('class', 'row');
-        rowDiv.setAttribute('display', 'flex');
-        rowDiv.setAttribute('align-items', 'stretch');
-    
         for (let j = 0; j < sideLength; j++) {
-            const cellDiv = document.createElement('div');
-            cellDiv.setAttribute('class', 'cell');
-            cellDiv.style.backgroundColor = 'darkgray';
-            cellDiv.style.height = '40px';
-            cellDiv.style.width = '40px';
-            cellDiv.style.border = '1px dotted gray';
-                
-            containerDiv.appendChild(cellDiv);
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.style.width = `${container.clientWidth / sideLength}px`;
+            cell.style.height = `${container.clientWidth / sideLength}px`;
+
+            cell.addEventListener('mouseover', () => {
+                cell.style.backgroundColor = 'rgb(54, 54, 54)';
+            });
+
+            container.appendChild(cell);
         }
-        containerDiv.appendChild(rowDiv);
     }
-    
-    const cells = document.querySelectorAll('div.cell');
-    for (const cell of cells) {
-        cell.addEventListener('mouseover', () => {
-            cell.style.backgroundColor = '#2E2E2E'; // dark gray
-        });
-    }
+
 }
 
 function attachButtonListeners(button) {
     button.addEventListener('mouseover', () => {
-        customizeButton.style.backgroundColor = 'rgb(211, 211, 186)';
+        button.style.backgroundColor = 'rgb(187, 187, 166)';
     });
 
     button.addEventListener('mouseout', () => {
-        customizeButton.style.backgroundColor = 'beige';
+        button.style.backgroundColor = 'beige';
     });
 
     button.addEventListener('click', () => {
